358ef3cd1c033e86c0c5ef941d76b1a7393665e5
AUCCode <marcopollomia@gmail.com>
2020-08-17 19:20:42 -0400

    working

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,84 +2,84 @@ var store = [{
         "title": "Booleans",
         "excerpt":"Booleans Booleans in Python are truth values, i.e. True or False. Booleans are generally used for control flow in programs. The 2 types of boolean values in Python are True and False Both boolean values must start with an Uppercase Letter True # correct False # correct false # wrong...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Booleans",
+        "url": "https://marcopollom.github.io//python/tutorial/Booleans",
         "teaser": null
       },{
         "title": "Collections",
         "excerpt":"Lists, Tuples, Dictionaries Lists, Tuples, Dictionaries are all collection data types, meaning that they can store multiple different items within one entry Here are some formal definitions of these data types and some examples: Lists are collections that are ordered, changeable, and allow duplicates. Tuples are collections that are ordered,...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Collections",
+        "url": "https://marcopollom.github.io//python/tutorial/Collections",
         "teaser": null
       },{
         "title": "Conditionals",
         "excerpt":"if and if-else Statements This page will cover the basics on if/else statements in Python. What are if/else statements? if/else statements are decision making statements that only execute if a certain condition is met. Decision making is a very useful tool when it comes to programming because it allows to...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Conditionals",
+        "url": "https://marcopollom.github.io//python/tutorial/Conditionals",
         "teaser": null
       },{
         "title": "Intro",
         "excerpt":"Introduction This series of lesson will be about the Python programming language. I will try to keep everything as beginner friendly as I can in these lessons. The only assumption I will make is that you know how to add :). If you want to follow along or program Python...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Intro",
+        "url": "https://marcopollom.github.io//python/tutorial/Intro",
         "teaser": null
       },{
         "title": "Logic",
         "excerpt":"Python allows us to control the flow of our program using logical operators and truth values: True and False. Operators Python also allows us to compare attributes of data by using the following: Equality (==) Greater than or equal (&gt;=) Less than or equal (&lt;=) Less than (&lt;) Greater than...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Logic",
+        "url": "https://marcopollom.github.io//python/tutorial/Logic",
         "teaser": null
       },{
         "title": "Loops",
         "excerpt":"Imagine we wanted to print out the first 100 positive integers (1, 2, 3, 4…). Till now, the only way we could do that is by manually printing each number: print(1) print(2) print(3) . . . As you can probably tell this get very messy and becomes hard to maintain...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Loops",
+        "url": "https://marcopollom.github.io//python/tutorial/Loops",
         "teaser": null
       },{
         "title": "Numbers",
         "excerpt":"Numbers The first basic data type we’ll cover are numbers There are a two different “types” of numbers in python: Integers - positive or negative whole numbers. Float - real numbers, but also can contain decimal points Here are some Examples Integers 1 57 2020 Floats 6.5 2.7e10 # Scientific...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Numbers",
+        "url": "https://marcopollom.github.io//python/tutorial/Numbers",
         "teaser": null
       },{
         "title": "Strings",
         "excerpt":"Strings Strings are sequences of characters in Python In short, Strings are/can be bits of text, numbers, and symbols that can be stored The common way of denoting a string is to surround it with quotation marks, double or single \"This is a string\" 'This is also a string' \"23\"...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Strings",
+        "url": "https://marcopollom.github.io//python/tutorial/Strings",
         "teaser": null
       },{
         "title": "Variables",
         "excerpt":"Variables Variables are one of the most important tools while programming in any language. Variables are containers that can store any data type, even other variables. They allow you to store and manipulate values, while also inheriting the properties of the data type of the data stored. In Python, declaring...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Variables",
+        "url": "https://marcopollom.github.io//python/tutorial/Variables",
         "teaser": null
       },{
         "title": "What is Python",
         "excerpt":"The Python Programming Language Python is a dynamically typed, interpreted, high-level programming language created by Guido van Rossum in 1990. Python has gained a lot of popularity over the years for how simple the language is to learn and by its ever expanding community. Python embraces the use of whitespace,...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/wisp",
+        "url": "https://marcopollom.github.io//python/tutorial/wisp",
         "teaser": null
       },{
         "title": "Hello",
         "excerpt":"Welcome to the Blog  ","categories": [],
         "tags": [],
-        "url": "/hello/",
+        "url": "https://marcopollom.github.io//hello/",
         "teaser": null
       },{
         "title": "Functions",
         "excerpt":"Functions in Python are reusable lines of code that only run once they are called on. Functions can be used to shorten the overall lines of code needed in a program, as well as modify and return data. The Syntax for functions is as follows: def my_function(): # Do stuff...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Functions",
+        "url": "https://marcopollom.github.io//python/tutorial/Functions",
         "teaser": null
       },{
         "title": "Linear Search And Max Value",
         "excerpt":"Since we have a good enough foundation with Python so far, I think it is time to introduce two simple algorithms that use most of what we have learned so far. Linear Search The first algorithm we will cover is Linear Search. Linear Search is the processes of checking if...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Linear-Search-and-Max-Value",
+        "url": "https://marcopollom.github.io//python/tutorial/Linear-Search-and-Max-Value",
         "teaser": null
       },{
         "title": "Linear Search And Max Value Continued",
         "excerpt":"Now that we know what functions are in Python and how we use them, why don’t we take a look back at our first examples and turn them into functions. Lets begin with Linear Search. Linear Search Function In our previous example (Example 1), we came up with a way...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "/python/tutorial/Linear-Search-and-Max-Value-Continued",
+        "url": "https://marcopollom.github.io//python/tutorial/Linear-Search-and-Max-Value-Continued",
         "teaser": null
       }]
