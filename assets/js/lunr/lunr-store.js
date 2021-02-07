3b15b8346109b8996d465cd4f2a53f0904724cee
AUCCode <marcopollomia@gmail.com>
2021-02-07 14:37:21 -0500

    Added Clock Article

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,138 +2,144 @@ var store = [{
         "title": "Booleans",
         "excerpt":"Booleans Booleans in Python are truth values, i.e. True or False. Booleans are generally used for control flow in programs. The 2 types of boolean values in Python are True and False Both boolean values must start with an Uppercase Letter True # correct False # correct false # wrong...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Booleans",
+        "url": "/python/tutorial/Booleans",
         "teaser": null
       },{
         "title": "Collections",
         "excerpt":"Lists, Tuples, Dictionaries Lists, Tuples, Dictionaries are all collection data types, meaning that they can store multiple different items within one entry Here are some formal definitions of these data types and some examples: Lists are collections that are ordered, changeable, and allow duplicates. Tuples are collections that are ordered,...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Collections",
+        "url": "/python/tutorial/Collections",
         "teaser": null
       },{
         "title": "Conditionals",
         "excerpt":"if and if-else Statements This page will cover the basics on if/else statements in Python. What are if/else statements? if/else statements are decision making statements that only execute if a certain condition is met. Decision making is a very useful tool when it comes to programming because it allows to...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Conditionals",
+        "url": "/python/tutorial/Conditionals",
         "teaser": null
       },{
         "title": "Intro",
         "excerpt":"Introduction This series of lesson will be about the Python programming language. I will try to keep everything as beginner friendly as I can in these lessons. The only assumption I will make is that you know how to add :). If you want to follow along or program Python...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Intro",
+        "url": "/python/tutorial/Intro",
         "teaser": null
       },{
         "title": "Logic",
         "excerpt":"Python allows us to control the flow of our program using logical operators and truth values: True and False. Operators Python also allows us to compare attributes of data by using the following: Equality (==) Greater than or equal (&gt;=) Less than or equal (&lt;=) Less than (&lt;) Greater than...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Logic",
+        "url": "/python/tutorial/Logic",
         "teaser": null
       },{
         "title": "Loops",
         "excerpt":"Imagine we wanted to print out the first 100 positive integers (1, 2, 3, 4…). Till now, the only way we could do that is by manually printing each number: print(1) print(2) print(3) . . . As you can probably tell this get very messy and becomes hard to maintain...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Loops",
+        "url": "/python/tutorial/Loops",
         "teaser": null
       },{
         "title": "Numbers",
         "excerpt":"Numbers The first basic data type we’ll cover are numbers There are a two different “types” of numbers in python: Integers - positive or negative whole numbers. Float - real numbers, but also can contain decimal points Here are some Examples Integers 1 57 2020 Floats 6.5 2.7e10 # Scientific...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Numbers",
+        "url": "/python/tutorial/Numbers",
         "teaser": null
       },{
         "title": "Strings",
         "excerpt":"Strings Strings are sequences of characters in Python In short, Strings are/can be bits of text, numbers, and symbols that can be stored The common way of denoting a string is to surround it with quotation marks, double or single \"This is a string\" 'This is also a string' \"23\"...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Strings",
+        "url": "/python/tutorial/Strings",
         "teaser": null
       },{
         "title": "Variables",
         "excerpt":"Variables Variables are one of the most important tools while programming in any language. Variables are containers that can store any data type, even other variables. They allow you to store and manipulate values, while also inheriting the properties of the data type of the data stored. In Python, declaring...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Variables",
+        "url": "/python/tutorial/Variables",
         "teaser": null
       },{
         "title": "What is Python",
         "excerpt":"The Python Programming Language Python is a dynamically typed, interpreted, high-level programming language created by Guido van Rossum in 1990. Python has gained a lot of popularity over the years for how simple the language is to learn and by its ever expanding community. Python embraces the use of whitespace,...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/wisp",
+        "url": "/python/tutorial/wisp",
         "teaser": null
       },{
         "title": "Hello",
         "excerpt":"Welcome to the Blog  ","categories": [],
         "tags": [],
-        "url": "https://marcopollom.github.io/hello/",
+        "url": "/hello/",
         "teaser": null
       },{
         "title": "Functions",
         "excerpt":"Functions in Python are reusable lines of code that only run once they are called on. Functions can be used to shorten the overall lines of code needed in a program, as well as modify and return data. The Syntax for functions is as follows: def my_function(): # Do stuff...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Functions",
+        "url": "/python/tutorial/Functions",
         "teaser": null
       },{
         "title": "Linear Search And Max Value",
         "excerpt":"Since we have a good enough foundation with Python so far, I think it is time to introduce two simple algorithms that use most of what we have learned so far. Linear Search The first algorithm we will cover is Linear Search. Linear Search is the processes of checking if...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Linear-Search-and-Max-Value",
+        "url": "/python/tutorial/Linear-Search-and-Max-Value",
         "teaser": null
       },{
         "title": "Linear Search And Max Value Continued",
         "excerpt":"Now that we know what functions are in Python and how we use them, why don’t we take a look back at our first examples and turn them into functions. Lets begin with Linear Search. Linear Search Function In our previous example (Example 1), we came up with a way...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Linear-Search-and-Max-Value-Continued",
+        "url": "/python/tutorial/Linear-Search-and-Max-Value-Continued",
         "teaser": null
       },{
         "title": "Files",
         "excerpt":"Python allows us to work with files natively, so we can store information for later, or read and analyze data . In this lesson, we will cover the basics of opening, reading, and writing to files in Python. File Basics Before we can start, it is good to know a...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Files",
+        "url": "/python/tutorial/Files",
         "teaser": null
       },{
         "title": "Intermediate Intro",
         "excerpt":"Introduction   Welcome to the intermediate Python Tutorial, where we will cover more advanced topics in Python.   Some of the topics that we’ll discuss are:      File Input and Output   Error Handling   Working with external modules   And much more!   Let’s Begin  ","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Intermediate-Intro",
+        "url": "/python/tutorial/Intermediate-Intro",
         "teaser": null
       },{
         "title": "Error Handling",
         "excerpt":"By now, you may have come across various errors in your Python programs. No worry, everyone makes mistakes the first time around. In this lesson, we will cover ways to handle and except errors in our Python programs. Error Examples There are many different error types in Python, each relating...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Error-Handling",
+        "url": "/python/tutorial/Error-Handling",
         "teaser": null
       },{
         "title": "String Formatting",
         "excerpt":"In this lesson we will cover different ways to format strings in Python. What if we wanted to create a function that greets a user when the function is called. Until now, we would have to manually concatenate each string using the + sign. This was ok for small strings,...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/String-Formatting",
+        "url": "/python/tutorial/String-Formatting",
         "teaser": null
       },{
         "title": "List Comprehension",
         "excerpt":"List comprehension allows us to easily fill out contents of a list in a single line of code. For example, if we wanted to fill a list with the first 5 even numbers, we would write something like this: l = [] for i in range(5): l.append(2 * i) print(l)...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/List-Comprehension",
+        "url": "/python/tutorial/List-Comprehension",
         "teaser": null
       },{
         "title": "Modules",
         "excerpt":"As we code more and more, our Python files tend to get larger and harder to maintain. Fortunately, Python allows us to split up our program into different files and import them as modules. Modules in Python are files that contain different functions that we can use within another Python...","categories": ["python"],
         "tags": ["tutorial"],
-        "url": "https://marcopollom.github.io/python/tutorial/Modules",
+        "url": "/python/tutorial/Modules",
         "teaser": null
       },{
         "title": "Controlling Motor With Raspberry Pi And Python",
         "excerpt":"In this lesson, we’re going see how we can control a DC motor using a Raspberry Pi and Python. What is a Raspberry Pi? “The Raspberry Pi is a low cost, credit-card sized computer that plugs into a computer monitor or TV, and uses a standard keyboard and mouse. It...","categories": [],
         "tags": [],
-        "url": "https://marcopollom.github.io/personalprojects/motors/Controlling-Motor-With-Raspberry-Pi-and-Python",
+        "url": "/personalprojects/motors/Controlling-Motor-With-Raspberry-Pi-and-Python",
         "teaser": null
       },{
         "title": "Personal Projects",
-        "excerpt":"\r \r \r     \r         Controlling Motors Using a Raspberry Pi and Python\r     \r \r     \r         Controlling a Seven Segment Display Using a Raspberry and Python\r     \r \r     More to Come!\r ","categories": [],
+        "excerpt":"\r \r \r     \r         Controlling Motors Using a Raspberry Pi and Python\r     \r \r     \r         Controlling a Seven Segment Display Using a Raspberry and Python\r     \r \r     \r         Creating a Clock with Raspberry Pi and Python\r     \r \r     More to Come!\r ","categories": [],
         "tags": [],
-        "url": "https://marcopollom.github.io/personalprojects",
+        "url": "/personalprojects",
         "teaser": null
       },{
         "title": "Controlling A Seven Segment Display With Raspberry Pi And Python",
         "excerpt":"In this lesson, we are going to cover how we can control a seven segment display using a Raspberry Pi and Python. If you are unfamiliar with what a Raspberry Pi or Python, please read my previous article on both found here. Getting Started Items Needed For this project, I...","categories": [],
         "tags": [],
-        "url": "https://marcopollom.github.io/personalprojects/SSDisplay/Controlling-A-Seven-Segment-Display-with-Raspberry-Pi-and-Python",
+        "url": "/personalprojects/SSDisplay/Controlling-A-Seven-Segment-Display-with-Raspberry-Pi-and-Python",
+        "teaser": null
+      },{
+        "title": "Creating A Clock With Raspberry Pi And Python",
+        "excerpt":"In this lesson, we are going to create a digital clock using a Raspberry Pi, Python, and a Seven Segment Display powered by a Max7219 chip. If you are unfamiliar with what a Raspberry Pi or Python, please read my previous article on both found here. Getting Started Items Needed...","categories": [],
+        "tags": [],
+        "url": "/personalprojects/SSDisplay/Creating-A-Clock-with-Raspberry-Pi-and-Python",
         "teaser": null
       }]
